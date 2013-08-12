afe83f7334cc27a99936b5952466574ac737329e
Kat Gatt <kgatti@ryerson.ca>
2013-08-12 19:12:17 +0000

    Update Gemfile.lock for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,3 @@ Category.find_or_create_by( name: "For Sale" )
 Category.find_or_create_by( name: "Community" ) 
 Category.find_or_create_by( name: "Jobs" )
 Category.find_or_create_by( name: "Housing" )
-
-
-1.upto(4) do |i|
-    Post.create(title: 'cats',
-                description: "A product.",
-                title: "Babysitting services",
-                price: 9.99
-                email: ''
-                city: i,
-                category_id: i)      
-    Post.create(price: 9.99,
-                description: "A product.",
-                title: "Dog Food",
-                location_id: i,
-                category_id: 3,
-                user_id: i)      
-    Post.create(price: 9.99,
-                description: "A product.",
-                title: "4 br home",
-                location_id: i,
-                category_id: 5,
-                user_id: i)      
-    Post.create(price: 9.99,
-                description: "A product.",
-                title: "Downtown apartment for rent",
-                location_id: i,
-                category_id: 6,
-                user_id: i)
-  end
